6a4ef204e6c5c5454349198223b419e8e7d4fbb3
RAHMAN-RS <abdulrahmanz07249@gmail.com>
2024-08-29 16:21:41 +0530

    Day 1

---
+++ Day 2/Script.js
@@ -0,0 +1,190 @@
+// Alert
+
+ alert("Email is not Found")
+
+// Prompt
+
+ let names= prompt("Enter the names")
+ console.log(names);
+
+ let numer= prompt("10,20,30")
+ console.log(number);
+
+
+
+
+// Arithmetic operater
+
+  let a=10;
+  let b=2;
+ 
+  console.log(a+b);
+  console.log(a-b);
+  console.log(a*b);
+  console.log(a/b);
+  console.log(a%b);
+  console.log(a**b);
+
+
+
+//Logical operater
+
+ let a=true;
+ let b=false;
+
+ console.log(a && b);
+ console.log(a || b);
+ console.log(!a);
+ console.log(!b);
+
+ let c=10;
+ let d=20;
+
+ console.log(c==10 && d==20);
+ console.log(c==20 && d==20);
+ console.log(c==10 || d==20);
+ console.log(c==20 || d==20);
+ console.log(!c==20);
+ console.log(!d==20);
+
+ 
+//Variables
+
+
+ var a=10;
+ let b=20;
+ const names="Rahman"
+
+ console.log(a,b,names);
+
+
+ var a= 10
+ var a= 100 //reassignment
+ var a=2000
+
+ console.log(a);
+ console.log(a+a);
+
+/let y=10
+ let z=20
+
+ console.log(y,z);
+ console.log(y+z);
+
+
+//Data Type
+
+//number
+//string
+//boolean
+//object
+
+
+ var a=10
+ console.log(a);
+
+ let b=20
+ console.log(b);
+
+ const names="Rahman"
+ console.log(names);
+
+
+ console.log(typeof a);
+
+ console.log(typeof b);
+
+ console.log(typeof names);
+
+ let c=true
+ console.log(typeof c);
+
+ let d=null
+ console.log(typeof d);
+
+
+//Object
+
+ let obj={name:"Rahman",age: 22 , native: "Mettupalayam"}
+
+
+ console.log(obj.age);
+
+
+ let obj={
+     name: "Abdul Rahman",
+     age: 22,
+     gmail: "abdulrahmanz07249@gmail.com",
+     password: 12345,
+     native: "Mettupalayam",
+
+ }
+
+    
+console.log(obj.name);
+ console.log(obj.age,);
+
+
+//Array
+
+
+ let arr=[10,20,30,40,50,60,70]
+
+console.log(arr[6]);
+
+
+ let arr=[
+    a=[10],b=[20],c=[30],d=[40],
+    
+ ]
+
+ console.log(arr[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+ 
+ 
+ 
+ 
+
+
+
+
